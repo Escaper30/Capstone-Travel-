f9ce49bc0d707c2acb5557d54c6466df43bdc53b
Escaper30 <113952069+Escaper30@users.noreply.github.com>
2022-10-11 21:24:32 -0400

    Update blog-details.php

--- blog-details.php
+++ blog-details.php
@@ -262,7 +262,10 @@
     <!-- Blog Details Section End -->
 
     <!-- Footer Section Begin -->
-    <footer class="footer">
+     <?php
+        include('footer.php');
+    ?>
+<!--     <footer class="footer">
         <div class="container">
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-6">
@@ -325,7 +328,7 @@
                 </div>
             </div>
         </div>
-    </footer>
+    </footer> -->
     <!-- Footer Section End -->
 
     <!-- Search Begin -->
