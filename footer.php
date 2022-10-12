2debca0efd705fffda363c2230df3ae79fe8f701
Dimple3009 <114263330+Dimple3009@users.noreply.github.com>
2022-10-11 22:40:34 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -16,10 +16,11 @@
                     <div class="footer__widget">
                         
                         <ul>
-                            <li><a href="#">Home</a></li>
+                            <li><a href="./index.php">Home</a></li>
                             <li><a href="#">Shop</a></li>
-                            <li><a href="#">Travel Stories</a></li>
-                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="./blog.php">Travel Stories</a></li>
+                            <li><a href="./contact.php">Contact Us</a></li>
+                            <li><a href="./about.php">About Us</a></li>
                         </ul>
                     </div>
                 </div>
