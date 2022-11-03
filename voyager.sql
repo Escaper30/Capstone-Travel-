3e432ef19721b8b1ef4010e808d06f03cc6f2f9b
Dimple3009 <114263330+Dimple3009@users.noreply.github.com>
2022-11-02 21:35:25 -0400

    Update voyager.sql

--- voyager.sql
+++ voyager.sql
@@ -38,10 +38,36 @@ ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
+
+CREATE TABLE `admin` (
+  `ad_id` int(11) NOT NULL,
+  `ad_fname` varchar(50) NOT NULL,
+  `ad_lname` varchar(50) NOT NULL,
+`ad_phone` int(10) NOT NULL,
+  `ad_email` varchar(50) NOT NULL,
+  `ad_password` varchar(50) NOT NULL,
+    `ad_confirm` varchar(50) NOT NULL 
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+INSERT INTO `admin` (`ad_id`, `ad_fname`, `ad_lname`, `ad_phone`, `ad_email`, `ad_password`, `ad_confirm`) VALUES
+(4, 'Ross', 'Geller', '5189964545', 'david@Gmail.com', '12345', '12345');
+
+
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`ad_id`);
+
+ALTER TABLE `admin`
+  MODIFY `ad_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 
 select *from user;
+select * from admin;
 
