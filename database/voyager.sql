4571e033feac5cd690c0d3220c9c4b6822ee094a
Escaper30 <113952069+Escaper30@users.noreply.github.com>
2022-11-08 14:02:09 -0500

    required upload

---
+++ database/voyager.sql
@@ -0,0 +1,93 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+DROP DATABASE IF EXISTS voyager;	-- c_dimple
+CREATE DATABASE voyager;	-- c_dimple
+USE voyager;	-- c_dimple
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+`country` varchar(50) NOT NULL,
+`phone` int(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+    `confirm` varchar(50) NOT NULL 
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+INSERT INTO `user` (`id`, `fname`, `lname`, `country`, `phone`, `email`, `password`, `confirm`) VALUES
+(4, 'David', 'garcia', 'canada', '5189964545', 'david@Gmail.com', '12345', '12345');
+
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+
+CREATE TABLE `admin` (
+  `ad_id` int(11) NOT NULL,
+  `ad_fname` varchar(50) NOT NULL,
+  `ad_lname` varchar(50) NOT NULL,
+`ad_phone` int(10) NOT NULL,
+  `ad_email` varchar(50) NOT NULL,
+  `ad_password` varchar(50) NOT NULL,
+    `ad_confirm` varchar(50) NOT NULL 
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+INSERT INTO `admin` (`ad_id`, `ad_fname`, `ad_lname`, `ad_phone`, `ad_email`, `ad_password`, `ad_confirm`) VALUES
+(4, 'Ross', 'Geller', '5189964545', 'david@Gmail.com', '12345', '12345');
+
+
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`ad_id`);
+
+ALTER TABLE `admin`
+  MODIFY `ad_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `message` varchar(50) NOT NULL
+    
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+INSERT INTO `contact` (`id`, `fname`, `lname`, `email`, `message`) VALUES
+(4, 'David', 'garcia','david@Gmail.com', 'nk');
+
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+select *from user;
+select * from admin;
+select * from contact;
+
---
+++ includes/header.php
@@ -0,0 +1,75 @@
+
+      <!-- Offcanvas Menu Begin/hamburger-->
+      <div class="offcanvas-menu-overlay"></div>
+    <div class="offcanvas-menu-wrapper">
+        <div class="offcanvas__option">
+            <div class="offcanvas__links">
+                <a href="#">Sign In/Log In</a>
+                <a href="#">FAQs</a>
+            </div>
+            <!-- <div class="offcanvas__top__hover">
+                <span>Usd <i class="arrow_carrot-down"></i></span>
+                <ul>
+                    <li>USD</li>
+                    <li>EUR</li>
+                    <li>USD</li>
+                </ul>
+            </div> -->
+        </div>
+        <div class="offcanvas__nav__option">
+            <a href="#" class="search-switch"><img src="img/icon/search.png" alt=""></a>
+            <a href="#"><img src="img/icon/heart.png" alt=""></a>
+            <a href="#"><img src="img/icon/cart.png" alt=""> <span>0</span></a>
+            <!-- <div class="price">$0.00</div> -->
+        </div>
+        <div id="mobile-menu-wrap"></div>
+        <div class="offcanvas__text">
+        <p>Join With <b>VOYAGER</b> For Latest Travel Stories</p>
+        </div>
+    </div>
+    <!-- Offcanvas Menu End -->
+
+    <!-- Header Section Begin -->
+    <header class="header">
+       
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3">
+                    <div class="header__logo">
+                        <a href="./index.php"><img src="img/logo_capstone/logo_head.png" alt="" height="35px" width="180px"></a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <nav class="header__menu mobile-menu">
+                        <ul>
+                            <li class="active"><a href="./index.php">Home</a></li>
+                            <li><a href="./shop.php">Shop</a></li>
+                            <!-- <li><a href="#">Pages</a>
+                                <ul class="dropdown">
+                                    <li><a href="./about.html">About Us</a></li>
+                                    <li><a href="./shop-details.html">Shop Details</a></li>
+                                    <li><a href="./shopping-cart.html">Shopping Cart</a></li>
+                                    <li><a href="./checkout.html">Check Out</a></li>
+                                    <li><a href="./blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li> -->
+                            <li><a href="./blog.php">Travel Stories</a></li>
+                            <li ><a href="./contact.php">Contact Us</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-lg-3 col-md-3">
+                    <div class="header__nav__option">
+                        <a href="#" class="search-switch"><img src="img/icon/search.png" alt=""></a>
+                        <a href="#"><img src="img/icon/heart.png" alt=""></a>
+                        <a href="#"><img src="img/icon/cart.png" alt=""> <span>0</span></a>
+                        <!-- <div class="price">$0.00</div> -->
+                    </div>
+                </div>
+            </div>
+            <div class="canvas__open"><i class="fa fa-bars"></i></div>
+        </div>
+    </header>
+    <!-- Header Section End -->
+
+  
