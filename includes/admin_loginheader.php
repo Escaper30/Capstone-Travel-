42a81e03657eb947e1e9604bd2d5ef0287a29248
NipulHaldar <113951969+NipulHaldar@users.noreply.github.com>
2022-11-23 14:08:23 -0500

    Add files via upload

---
+++ includes/admin_loginheader.php
@@ -0,0 +1,27 @@
+<div class="header__top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-7">
+                        <div class="header__top__left">
+                        <p>Join With <b>VOYAGER</b> For Latest Travel Stories</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-5">
+                        <div class="header__top__right">
+                            <div class="header__top__links">
+                            <a href="#">Welcome <?php echo $admin["ad_fname"];  ?>&#9973;</a><a href="./logout_admin.php">Log Out</a>
+<!--                                 <a href="./faq.php">FAQs</a> -->
+                            </div>
+                            <div class="header__top__hover">
+                                <!-- <span>Usd <i class="arrow_carrot-down"></i></span>
+                                <ul>
+                                    <li>USD</li>
+                                    <li>EUR</li>
+                                    <li>USD</li>
+                                </ul> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>  
