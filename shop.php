d387788277a5aebecc5e9d2a62ddb42dfbaa0508
NipulHaldar <113951969+NipulHaldar@users.noreply.github.com>
2022-12-12 21:34:16 -0500

    Update shop.php

--- shop.php
+++ shop.php
@@ -1,103 +1,131 @@
 <?php
-	session_start();
-	//initialize cart if not set or is unset
-	if(!isset($_SESSION['cart'])){
-		$_SESSION['cart'] = array();
-	}
+require 'db_conn.php';
 
-	//unset qunatity
-	unset($_SESSION['qty_array']);
-?>
+$select = new Select();
+
+if(!empty($_SESSION["id"])){
+  $user = $select->selectUserById($_SESSION["id"]);
+  include('includes/loggedin_header.php');
+ 
+}
+// else if(empty($_SESSION["id"])){
+//     header("Location: index.php");
+// }
+else{
+//   header("Location: register_user.php");
+  include('includes/top_header.php');
+}
+?> 
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Shop | Voyager</title>
+	<title>Cart | Voyager</title>
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-	<style>
-		.product_image{
-			height:200px;
-		}
-		.product_name{
-			height:80px; 
-			padding-left:20px; 
-			padding-right:20px;
-		}
-		
-	</style>
 </head>
 <body>
 <?php
-include('includes/shopheader.php')
-?>
-
+        include('includes/shopheader.php');
+    ?>
 <div class="container">
+	<!-- <nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">Simple Shopping Cart</a>
+	    </div>
 
-	<?php
-	
-		//info message
-		if(isset($_SESSION['message'])){
-			?>
-			<div class="row">
-				<div class="col-sm-6 col-sm-offset-6">
-					<div class="alert alert-info text-center">
-						<?php echo $_SESSION['message']; ?>
-					</div>
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	      	left nav here 
+	      </ul>
+	      <ul class="nav navbar-nav navbar-right">
+	      	<li class="active"><a href="view_cart.php"><span class="badge"><?php echo count($_SESSION['cart']); ?></span> Cart <span class="glyphicon glyphicon-shopping-cart"></span></a></li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav> -->
+	<h1 class="page-header text-center">Cart Details</h1>
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			<?php 
+			if(isset($_SESSION['message'])){
+				?>
+				<div class="alert alert-info text-center">
+					<?php echo $_SESSION['message']; ?>
 				</div>
-			</div>
-			<?php
-			unset($_SESSION['message']);
-		}
-		//end info message
-		//fetch our products	
-		//connection
-		$conn = new mysqli('localhost', 'root', '', 'voyager');
+				<?php
+				unset($_SESSION['message']);
+			}
 
-		$sql = "SELECT * FROM products";
-		$query = $conn->query($sql);
-		$inc = 4;
-		while($row = $query->fetch_assoc()){
-			$inc = ($inc == 4) ? 1 : $inc + 1; 
-			if($inc == 1) echo "<div class='row text-center'>";  
 			?>
-			
-			
-                        <div class="col-lg-3 col-md-6 col-sm-6">
-                            <div class="product__item">
-                                <div class="product__item__pic set-bg">
-								<img src="<?php echo $row['pr_image'] ?>" width="auto" height="auto">
-                                   
-                                </div>
-                                <div class="product__item__text">
-                                    <h6><?php echo $row['pr_title']; ?></h6>
-                                    <a  class="add-cart" href="add_cart.php?id=<?php echo $row['pr_id']; ?>" >+ Add To Cart</a>
-                                   
-                                    <h5><b>$<?php echo $row['pr_cost']; ?></b></h5>
-                                    
-                                </div>
-                            </div>
-                        </div>
-			
-			<?php
-		}
-		if($inc == 1) echo "<div></div><div></div><div></div></div>"; 
-		if($inc == 2) echo "<div></div><div></div></div>"; 
-		if($inc == 3) echo "<div></div></div>";
-		
-		//end product row 
-	
-	?>
-</div>
+			<form method="POST" action="save_cart.php">
+			<table class="table table-bordered table-striped">
+				<thead>
+					<th></th>
+					<th>Name</th>
+					<th>Price</th>
+					<th>Persons</th>
+					<th>Subtotal</th>
+				</thead>
+				<tbody>
+					<?php
+						//initialize total
+						$total = 0;
+						if(!empty($_SESSION['cart'])){
+						//connection
+						$conn = new mysqli('localhost', 'root', '', 'voyager');
+						//create array of initail qty which is 1
+ 						$index = 0;
+ 						if(!isset($_SESSION['qty_array'])){
+ 							$_SESSION['qty_array'] = array_fill(0, count($_SESSION['cart']), 1);
+ 						}
+						$sql = "SELECT * FROM products WHERE pr_id IN (".implode(',',$_SESSION['cart']).")";
+						$query = $conn->query($sql);
+							while($row = $query->fetch_assoc()){
+								?>
+								<tr>
+									<td>
+										<a href="delete_item.php?id=<?php echo $row['pr_id']; ?>&index=<?php echo $index; ?>" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash"></span></a>
+									</td>
+									<td><?php echo $row['pr_title']; ?></td>
+									<td><?php echo number_format($row['pr_cost'], 2); ?></td>
+									<input type="hidden" name="indexes[]" value="<?php echo $index; ?>">
+									<td><input type="text" class="form-control" value="<?php echo $_SESSION['qty_array'][$index]; ?>" name="qty_<?php echo $index; ?>"></td>
+									<td><?php echo number_format($_SESSION['qty_array'][$index]*$row['pr_cost'], 2); ?></td>
+									<?php $total += $_SESSION['qty_array'][$index]*$row['pr_cost']; ?>
+								</tr>
+								<?php
+								$index ++;
+							}
+						}
+						else{
+							?>
+							<tr>
+								<td colspan="4" class="text-center">No Item in Cart</td>
+							</tr>
+							<?php
+						}
+
+					?>
+					<tr>
+						<td colspan="4" align="right"><b>Total</b></td>
+						<td><b><?php echo number_format($total, 2); ?></b></td>
+					</tr>
+				</tbody>
+			</table>
+			<a href="index.php" class="btn btn-dark" style="font-size:12px;><span class="glyphicon glyphicon-arrow-left"></span> Back</a>
+			<button type="submit" class="btn btn-success" style="font-size:12px;" name="save">Save Changes</button>
+			<a href="clear_cart.php" class="btn btn-danger" style="font-size:12px;><span class="glyphicon glyphicon-trash"></span> Clear Cart</a>
+			<br><br><a href="checkout.php" class="primary-btn"><span class="glyphicon glyphicon-check"></span> Checkout</a>
+			</form>
+		</div>
 	</div>
-
-	
-	<?php
-include('includes/shopfooter.php')
-?>
-
-
-                       
-
+</div>
 </body>
-
 </html>
